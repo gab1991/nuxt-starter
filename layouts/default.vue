970d195ecf7ad6b47ab7924e39d854e9a747ebac
Igor Kornilov <gaber32188@gmail.com>
2024-01-07 18:56:44 +0200

    Add default layout and about page

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+	<nav class="p-4 h-20 flex items-center justify-between gap-10 bg-orange-200">
+		<span>Music of your dreams</span>
+		<ul class="flex items-center gap-5">
+			<li><NuxtLink class="font-bold hover:text-lg transition-all" to="/">Home</NuxtLink></li>
+			<li><NuxtLink class="font-bold hover:text-lg transition-all" to="/about">About</NuxtLink></li>
+		</ul>
+	</nav>
+	<main>
+		<slot />
+	</main>
+</template>
