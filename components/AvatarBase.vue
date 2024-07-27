d41f5987cd954db01339b4003bd26f9e89bb5657
Igor Kornilov <gaber32188@gmail.com>
2024-07-27 12:29:09 +0300

    feat: Add AvatarBase component for displaying user avatar in NavBar

---
+++ components/AvatarBase.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="h-[100px] w-[100px] rounded-full bg-orange-300"></div>
+</template>
--- components/NavBar.vue
+++ components/NavBar.vue
@@ -3,6 +3,7 @@ import HomeSvg from "~/assets/svg/Home.svg";
 import UsersSvg from "~/assets/svg/UsersOutline.svg";
 import MailSvg from "~/assets/svg/MailOutline.svg";
 import BellSvg from "~/assets/svg/BellOutline.svg";
+import AvatarBase from "./AvatarBase.vue";
 
 const LINKS = [
   { name: "HOME", path: "/", icon: HomeSvg },
@@ -13,16 +14,22 @@ const LINKS = [
 </script>
 
 <template>
-  <CardBase is="nav" class="flex flex-col gap-2">
-    <NuxtLink
-      v-for="link in LINKS"
-      :key="link.path"
-      class="flex w-full items-center gap-2 rounded-xl px-4 py-2 text-xl text-text transition-all duration-200 hover:bg-secondary-30"
-      :to="link.path"
-      :active-class="'bg-secondary-30'"
-    >
-      <component :is="link.icon" filled />
-      {{ link.name }}
-    </NuxtLink>
+  <CardBase is="nav" class="flex flex-col gap-28">
+    <div class="flex flex-col gap-3">
+      <AvatarBase />
+      <span class="text-lg">Roberto Carlos</span>
+    </div>
+    <nav class="flex flex-col gap-2">
+      <NuxtLink
+        v-for="link in LINKS"
+        :key="link.path"
+        class="flex w-full items-center gap-2 rounded-xl px-4 py-2 text-xl text-text transition-all duration-200 hover:bg-secondary-30"
+        :to="link.path"
+        :active-class="'bg-secondary-30'"
+      >
+        <component :is="link.icon" filled />
+        {{ link.name }}
+      </NuxtLink>
+    </nav>
   </CardBase>
 </template>
